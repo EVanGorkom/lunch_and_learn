cd21fc06ae99ff8d94783de5d85e50fda3956424
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-11 17:14:33 -0600

    Create the find_country method for the country facade

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -9,4 +9,13 @@ class CountryFacade
   def fetch_countries
     all_countries = CountryService.new.find_all
   end
+
+  def find_country(country_name)
+    country = CountryService.new.search_country(country_name.downcase)
+    if country[0] == nil
+      nil
+    else
+      country[0]["name"]["common"]
+    end
+  end
 end
